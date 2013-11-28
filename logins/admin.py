68ba66ad419b1d5cad558d65f9aefd9afa71ba08
yudasinal <yudasinal1@gmail.com>
2013-11-28 23:40:37 +0100

    Added inline and modelAdmin

--- logins/admin.py
+++ logins/admin.py
@@ -4,6 +4,11 @@ from logins.models import Department, Game
 class GameInline(admin.StackedInline):
     model = Game
     extra = 1
+    '''
+    def game_namagame(object):
+     return object.game.namegame
+    '''
+
 
 class DepartmentAdmin(admin.ModelAdmin):
     fieldsets = [
@@ -11,6 +16,19 @@ class DepartmentAdmin(admin.ModelAdmin):
     ]
     inlines = [GameInline]
 
+    '''
+    list_display = ['game_namegame',]
+    '''
+     
+    list_filter = ['name']
+    search_fields = ['name']
+
+    '''
+    def name_of_the_game(self):
+        return self.game.name_of_the_game
+    '''
+
 
 admin.site.register(Department, DepartmentAdmin)
+admin.site.register(Game)
 
