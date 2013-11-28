1cd30424870cfb91cf15506f8aab940ae3a30143
yudasinal <yudasinal1@gmail.com>
2013-11-28 23:42:27 +0100

    Added the login form to urls's

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,12 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
+from django.contrib.auth import login
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'mysite.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^logins/', include('logins.urls')),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', 'logins.views.login_user'),
+    
 )
+
+
