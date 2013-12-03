b5b612bb0ad94eefd06e9c2eeb393e3f50ae5719
Liubou Yudasina <yudasinal1@gmail.com>
2013-12-03 12:28:17 +0100

    Implemented the built-in login

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,10 +6,9 @@ from django.contrib.auth import login
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^logins/', include('logins.urls')),
+    url(r'^logins/', include('logins.urls', namespace = "logins")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'logins.views.login_user'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth.html' }),
     
 )
-
 
