eb7a3dea3c4bb29c16fe1f858611149d7440bfd1
Liubou Yudasina <yudasinal1@gmail.com>
2013-12-03 17:58:39 +0100

    Edited urls for detail view

--- logins/urls.py
+++ logins/urls.py
@@ -6,7 +6,7 @@ urlpatterns = patterns('',
     # ex: /polls/
     url(r'^$', views.index, name='index'),
     # ex: /polls/5/
-    url(r'^(?P<department_id>\d+)/$', views.detail, name='detail'),
+    url(r'^(?P<info_id>\d+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
 
 )
