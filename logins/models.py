b22971f3e341ff6a0c69466279a56f451199c6ab
Liubou Yudasina <yudasinal1@gmail.com>
2013-12-03 17:59:12 +0100

    Created new models

--- logins/models.py
+++ logins/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.auth.models import User, UserManager
+
 
 class Department(models.Model):
     name = models.CharField(max_length=200)
@@ -6,10 +8,24 @@ class Department(models.Model):
         return self.name
     
 class Game(models.Model):
-    department = models.ForeignKey(Department) 
     name_of_the_game = models.CharField(max_length=200)
     def __unicode__(self):  
         return self.name_of_the_game
+
+class Info(models.Model):
+	organization_name = models.CharField(max_length=200)
+	user_name = models.CharField(max_length=200)
+	password = models.CharField(max_length=200)
+	game = models.ManyToManyField(Game)
+	department = models.ManyToManyField(Department)
+	def __unicode__(self):
+		 return self.organization_name+ ': '+ 'user name: ' +self.user_name+ ', '+ 'password: ' + self.password
+
+
+class CustomUser(User):
+	department = models.ManyToManyField(Department)
+	game = models.ManyToManyField(Game)
+	objects = UserManager()
 
 
 
