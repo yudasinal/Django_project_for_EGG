7441455b9669e94ff9988d3f1179efb86dbbf40f
Liubou Yudasina <yudasinal1@gmail.com>
2013-12-06 13:20:40 +0100

    Added the CustomUser section into the admin

--- logins/admin.py
+++ logins/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from logins.models import *
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+
 
 class InfoInline(admin.StackedInline):
     model = Info.game.through
@@ -9,9 +12,34 @@ class InfoDInline(admin.StackedInline):
     model = Info.department.through
     extra = 1
 
+class InfoAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,              {'fields':['organization_name', 'user_name', 'password']}),
+    ]
+    inlines = [InfoInline, InfoDInline]
+
+class CustomUserInline(admin.StackedInline):
+    model = CustomUser
+    can_delete = False
+    verbose_name_plural = 'custom_user'
+
+# Define a new User admin
+class UserAdmin(UserAdmin):
+    inlines = (CustomUserInline, )
+
+# Re-register UserAdmin
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)    
+admin.site.register(Department)
+admin.site.register(Game)
+admin.site.register(Info, InfoAdmin)
+
+'''
 class CustomUserAdmin(admin.ModelAdmin): 
     list_display = ('first_name', 'last_name', 'department', 'game')   #How to display ManyToMany? list_display does not handle it
 admin.site.register(CustomUser, CustomUserAdmin)
+
+'''
 
 '''
 class DepartmentAdmin(admin.ModelAdmin):
@@ -24,13 +52,8 @@ class DepartmentAdmin(admin.ModelAdmin):
     search_fields = ['name']
 '''
 
-class InfoAdmin(admin.ModelAdmin):
-    fieldsets = [
-        (None,              {'fields':['organization_name', 'user_name', 'password']}),
-    ]
-    inlines = [InfoInline, InfoDInline]
 
-admin.site.register(Department)
-admin.site.register(Game)
-admin.site.register(Info, InfoAdmin)
+
+
+
 
