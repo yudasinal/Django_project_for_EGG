0450ce1f90e4c068987eb4fa675e86d173f91ef6
Liubou Yudasina <yudasinal1@gmail.com>
2013-12-06 17:29:12 +0100

    Changed the urls for login page

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,14 +1,15 @@
 from django.conf.urls import patterns, include, url
+from logins.views import login
 
 from django.contrib import admin
-from django.contrib.auth import login
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^logins/', include('logins.urls', namespace = "logins")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'auth.html' }),
+    url(r'^$', login, name='default'),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls'))
     
 )
 
