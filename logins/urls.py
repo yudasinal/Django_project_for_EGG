d3c98304ff2dce1218f77af44aa29e9563d57e85
Liubou Yudasina <yudasinal1@gmail.com>
2013-12-10 16:53:54 +0100

    Created urls for login, logout and registration

--- logins/urls.py
+++ logins/urls.py
@@ -8,5 +8,8 @@ urlpatterns = patterns('',
     # ex: /polls/5/
     url(r'^(?P<info_id>\d+)/$', views.detail, name='detail'),
     # ex: /polls/5/results/
+    url(r'^create/$', views.create, name='create'),
+    url(r'^register/$', views.register_user, name='register'),
+    url(r'^register_success/$', views.register_success, name='register_success'),
 
 )
