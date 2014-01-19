cd25c37e80f404d86ba0495e5c7be343e191033d
Liubou Yudasina <yudasinal1@gmail.com>
2014-01-20 00:37:17 +0100

    Little change

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-LOGIN_REDIRECT_URL = '/login'
+LOGIN_REDIRECT_URL = '/logins'
 
 LOGIN_URL = '/login'
 LOGOUT_URL = '/logout'
