fde6951f35d5455569398eb538cfc72dae88927f
Liubou Yudasina <yudasinal1@gmail.com>
2014-03-19 20:41:38 +0100

    Update urls.py

--- logins/urls.py
+++ logins/urls.py
@@ -5,11 +5,8 @@ from logins.views import InfoEdit
 from logins import views
 
 urlpatterns = patterns('',
-    # ex: /polls/
     url(r'^$', views.index, name='index'),
-    # ex: /polls/5/
     url(r'^(?P<info_id>\d+)/$', views.detail, name='detail'),
-    # ex: /polls/5/results/
     url(r'^create/$', views.create, name='create'),
     url(r'^register/$', views.register_user, name='register'),
     url(r'^register_success/$', views.register_success, name='register_success'),
